947cba7ea0095acbbe5dd0f86d63104088deeccc
Marcos Della Justina Schulz <markosdjs@gmail.com>
2022-02-11 11:39:02 -0300

    Adicionado classe departamento

---
+++ src/application/Program.java
@@ -0,0 +1,13 @@
+package application;
+
+import domain.model.Departamento;
+
+public class Program {
+
+    public static void main (String [] args){
+
+        Departamento test = new Departamento( 1 , "teste");
+
+        System.out.println(test);
+    }
+}
